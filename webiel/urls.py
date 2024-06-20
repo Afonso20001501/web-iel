dd3a3e6823a9964b4c1819f36dd923d23d4c1362
Afonso Miqueias <afonsogroove@gmail.com>
2024-06-20 21:58:26 +0100

    front-terminado

--- webiel/urls.py
+++ webiel/urls.py
@@ -5,5 +5,20 @@ from . import views
 app_name = 'webiel'
 
 urlpatterns = [
-    path('', views.home, name="home"),  # Home 
+    path('', views.home, name="home"), # Home 
+    path('sobre/', views.sobre, name="sobre_historial"),# sobre_historial
+    path('sobre_missao/', views.sobre_missao, name="sobre_missao"),# sobre_missão
+    path('sobre_estatuto/', views.sobre_estatuto, name="sobre_estatuto"),# sobre_estatuto
+    path('sobre_pacto/', views.sobre_pacto, name="sobre_pacto"),# sobre_Pacto
+    path('sobre_fe/', views.sobre_fe, name="sobre_fe"),# sobre_fé
+    path('sobre_pratica/', views.sobre_pratica, name="sobre_pratica"),# sobre_pratica
+    path('noticia_iel/', views.noticia_iel, name="noticia_iel"),# Noticia_iel
+    path('contato/',  views.contato, name="contato"),# Contato
+    path('mocidade/',  views.mocidade, name="mocidade"),# Mocidade
+    path('senhoras/',  views.senhoras, name="senhoras"),# Senhoras
+    path('devem/',  views.devem, name="devem"),# Devem
+    path('homens/',  views.homens, name="homens"),# Homens
+    path('musica/',  views.musica, name="musica")# Musica
 ]
+
+
--- webiel/views.py
+++ webiel/views.py
@@ -3,3 +3,42 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'webiel/pages/index.html')
+
+def sobre(request):
+    return render(request, 'webiel/pages/sobre_historial.html')
+
+def sobre_missao(request):
+    return render(request,  'webiel/pages/sobre_missao.html')
+
+def sobre_estatuto(request):
+    return render(request,  'webiel/pages/sobre_estatuto.html')
+
+def sobre_pacto(request):
+    return render(request,  'webiel/pages/sobre_pacto.html')
+
+def sobre_fe(request):
+    return render(request,  'webiel/pages/sobre_fe.html')
+
+def sobre_pratica(request):
+    return render(request,  'webiel/pages/sobre_pratica.html')
+
+def noticia_iel(request):
+    return render(request,  'webiel/pages/noticia_iel.html')
+
+def contato(request):
+    return render(request,  'webiel/pages/contato.html')
+
+def mocidade(request):
+    return render(request,  'webiel/pages/mocidade.html')
+
+def senhoras(request):
+    return render(request,  'webiel/pages/senhoras.html')
+
+def devem(request):
+    return render(request,  'webiel/pages/devem.html')
+
+def homens(request):
+    return render(request,  'webiel/pages/homens.html')
+
+def musica(request):
+    return render(request,  'webiel/pages/musica.html')
